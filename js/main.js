521b52857d9157b6bb5d9cb954ea33a817c74a4b
JensAndreasson <jensandreasson77@gmail.com>
2019-06-04 19:04:35 +0200

    some html fixes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-function getHash(str, algo = "SHA-256") {
-    let strBuf = new TextEncoder('utf-8').encode(str);
-    return crypto.subtle.digest(algo, strBuf)
-        .then(hash => {
-            window.hash = hash;
-            // here hash is an arrayBuffer, 
-            // so we'll connvert it to its hex version
-            let result = '';
-            const view = new DataView(hash);
-            for (let i = 0; i < hash.byteLength; i += 4) {
-                result += ('00000000' + view.getUint32(i).toString(16)).slice(-8);
-            }
-            return result;
-        });
-}
-
 let h = [];
 
 let canvas = document.createElement('canvas');
@@ -25,10 +9,21 @@ let size = 16;
 canvas.width = size * 8;
 canvas.height = size * 8;
 
+let input = document.getElementById("string");
+
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        getHash(event.srcElement.value)
+            .then(hash => {
+                draw(hash, stringToColour(event.srcElement.value));
+            });
+    }
+});
+
 function draw(hash, color) {
     let column = 0;
     let row = 0;
-    //ctx.fillStyle = 'grey';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     hash = Array.from(hash);
     hash.forEach(element => {
@@ -48,43 +43,15 @@ function draw(hash, color) {
 
     let img = document.createElement("img");
     img.src = canvas.toDataURL();
+    img.classList.add("shadow-sm");
     imgdiv.appendChild(img);
-
 }
 
-//body.appendChild(canvas);
-
-
-let input = document.getElementById("string");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        getHash(event.srcElement.value)
-            .then(hash => {
-                draw(hash, stringToColour(event.srcElement.value));
-            });
-    }
-});
-/*
-getHash('fesk')
-    .then(hash => {
-        console.log(hash);
-        draw(hash);
-    });
-*/
 function isEven(char) {
     return String(char / 2).indexOf('.') === -1;
 }
 
-function makeRandomColor() {
-    return '#' + Math.random().toString(16).slice(-6);
-}
-
+//https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
 var stringToColour = function (str) {
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
@@ -96,4 +63,21 @@ var stringToColour = function (str) {
         colour += ('00' + value.toString(16)).substr(-2);
     }
     return colour;
+}
+
+//https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+function getHash(str, algo = "SHA-256") {
+    let strBuf = new TextEncoder('utf-8').encode(str);
+    return crypto.subtle.digest(algo, strBuf)
+        .then(hash => {
+            window.hash = hash;
+            // here hash is an arrayBuffer, 
+            // so we'll connvert it to its hex version
+            let result = '';
+            const view = new DataView(hash);
+            for (let i = 0; i < hash.byteLength; i += 4) {
+                result += ('00000000' + view.getUint32(i).toString(16)).slice(-8);
+            }
+            return result;
+        });
 }
